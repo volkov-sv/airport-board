7b093c4ddfbd14d1a3a513708a39bac005ee2872
volkov-sv <v0lk0v-sv@yandex.ru>
2019-01-12 22:01:01 +0300

    Update ticker.js

--- ticker.js
+++ ticker.js
@@ -16,10 +16,7 @@ setInterval(ticker.tick, 1000);
 //соответственно контекст при этом теряется
 //можно исправить, например, сделав обертку, чтобы вызвать как метод объекта и получить нужный контекст
 
-setInterval(function() {
-    ticker.tick();
-}
-    , 1000);
+setInterval(function() {ticker.tick()}, 1000);
 
  //или с помощью bind, чтобы передать контекст
  setInterval(ticker.tick.bind(ticker), 1000);
